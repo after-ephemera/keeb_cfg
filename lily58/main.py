3733b645a105ff32b43c0521571c6c66a684e6c4
Jk Jensen <jk@mystenlabs.com>
2025-02-05 10:32:31 -0700

    revert back to old layout, come on man. you can remember three layouts

--- lily58/main.py
+++ lily58/main.py
@@ -8,7 +8,12 @@ from kmk.modules.tapdance import TapDance
 from kmk.modules.holdtap import HoldTap
 from kmk.modules.combos import Combos
 from kmk.hid import HIDModes
-from kmk.extensions.peg_oled_Display import Oled,OledDisplayMode,OledReactionType,OledData
+from kmk.extensions.peg_oled_Display import (
+    Oled,
+    OledDisplayMode,
+    OledReactionType,
+    OledData,
+)
 from kmk.extensions.peg_rgb_matrix import Rgb_matrix
 from kmk.modules.split import Split
 from kmk.modules.sticky_keys import StickyKeys
@@ -31,24 +36,34 @@ keyboard.modules.append(sticky_keys)
 # oled
 oled_ext = Oled(
     OledData(
-      corner_one={0:OledReactionType.STATIC,1:["layer:"]},
-      corner_two={0:OledReactionType.LAYER,1:["1","2","3","4","5","6","7","8"]},
-      corner_three={0:OledReactionType.LAYER,1:["base","raise","lower","adjust","5","6","7","8"]},
-      corner_four={0:OledReactionType.LAYER,1:["main","nav","special","4","5","6","7","8"]}
+        corner_one={0: OledReactionType.STATIC, 1: ["layer:"]},
+        corner_two={
+            0: OledReactionType.LAYER,
+            1: ["1", "2", "3", "4", "5", "6", "7", "8"],
+        },
+        corner_three={
+            0: OledReactionType.LAYER,
+            1: ["base", "raise", "lower", "adjust", "5", "6", "7", "8"],
+        },
+        corner_four={
+            0: OledReactionType.LAYER,
+            1: ["main", "nav", "special", "4", "5", "6", "7", "8"],
+        },
     ),
-  toDisplay=OledDisplayMode.TXT,flip=False
+    toDisplay=OledDisplayMode.TXT,
+    flip=False,
 )
 # # oled
 keyboard.extensions.append(oled_ext)
-# KMK uses the final character in the name of the drive (luluL or luluR) to 
+# KMK uses the final character in the name of the drive (luluL or luluR) to
 # determine the side.
 split = Split(use_pio=True)
 keyboard.modules.append(split)
 
 LOWER = KC.MO(1)
 RAISE = KC.MO(2)
-DLFT = KC.LCTL(KC.LEFT) # Desktop left
-DRGT = KC.LCTL(KC.RIGHT) # Desktop right
+DLFT = KC.LCTL(KC.LEFT)  # Desktop left
+DRGT = KC.LCTL(KC.RIGHT)  # Desktop right
 # tap once for tab, hold for cmd, double tap for cmd + tab
 LGUIPP = KC.TD(KC.HT(KC.TAB, KC.LGUI), KC.LGUI(KC.TAB))
 # hold for control, double tap for control + tab
@@ -62,14 +77,14 @@ LALT = KC.HT(KC.PLUS, KC.LALT)
 F = KC.TD(KC.HT(KC.F, KC.LGUI, prefer_hold=False), KC.LGUI(KC.TAB))
 # tap for j, hold for cmd
 J = KC.HT(KC.J, KC.RGUI)
-# keymap
+# fmt: off
 keyboard.keymap = [ 
         [# qwerty
             KC.TAB,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,                     KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.GRAVE,
             LALT,    KC.Q,    KC.W,    KC.E,    KC.R,    KC.T,                      KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,    KC.MINUS,
-            KC.ESC,  KC.A,    KC.S,    KC.D,    F,    KC.G,                      KC.H,    J,    KC.K,    KC.L,    KC.SCLN, KC.QUOTE,
-            KC.TRNS, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,  LBRC,  RBRC, KC.N,    KC.M,    KC.COMM,  KC.DOT,  KC.SLSH, KC.BSLS, 
-                                       LOWER,   LCTLPP, KC.SK(KC.LSFT), KC.ENT,   KC.SPC,  KC.BSPC, RAISE,   KC.RGUI
+            KC.ESC,  KC.A,    KC.S,    KC.D,    KC.F,    KC.G,                      KC.H,    KC.J,    KC.K,    KC.L,    KC.SCLN, KC.QUOTE,
+            KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V,    KC.B,  LBRC,  RBRC, KC.N,    KC.M,    KC.COMM,  KC.DOT,  KC.SLSH, KC.BSLS, 
+                                       LOWER,   LCTLPP, KC.SK(KC.LGUI), KC.ENT,   KC.SPC,  KC.BSPC, RAISE,   KC.RGUI
         ], 
 
         [ # lower
@@ -93,7 +108,8 @@ keyboard.keymap = [
 [KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS], 
 [KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS], 
 [KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS], 
-[KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS] ] 
+[KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS,KC.TRNS] 
+] 
 
 # keymap
 if __name__ == '__main__': 
